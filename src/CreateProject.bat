f55348bd92867464e2b148b5e85c8c20b75c1117
CockroachSoulful <cockroach888@outlook.com>
2022-12-06 08:09:09 +0800

    feat: 增加基于.Net 7的AspNetCoreNet7Touch应用示例。

--- src/CreateProject.bat
+++ src/CreateProject.bat
@@ -68,6 +68,27 @@ dotnet sln GSA2TENET4StarTrek.dotNetFoundationTouch.sln add -s AspNetCoreTouch A
 @echo.
 @echo.
 @echo.
+@echo\&echo  ---------- AspNetCoreNet7Touch ----------
+dotnet new web -lang "C#" -f net7.0 --exclude-launch-settings --no-https -n AspNetCore4Empty -o AspNetCoreNet7Touch/AspNetCore4Empty/src
+dotnet sln GSA2TENET4StarTrek.dotNetFoundationTouch.sln add -s AspNetCoreNet7Touch AspNetCoreNet7Touch/AspNetCore4Empty/src
+
+dotnet new mvc -lang "C#" -f net7.0 --exclude-launch-settings --no-https -n AspNetCore4MVC -o AspNetCoreNet7Touch/AspNetCore4MVC/src
+dotnet sln GSA2TENET4StarTrek.dotNetFoundationTouch.sln add -s AspNetCoreNet7Touch AspNetCoreNet7Touch/AspNetCore4MVC/src
+
+dotnet new webapp -lang "C#" -f net7.0 --exclude-launch-settings --no-https -n AspNetCore4WebApp -o AspNetCoreNet7Touch/AspNetCore4WebApp/src
+dotnet sln GSA2TENET4StarTrek.dotNetFoundationTouch.sln add -s AspNetCoreNet7Touch AspNetCoreNet7Touch/AspNetCore4WebApp/src
+
+dotnet new webapi -lang "C#" -f net7.0 --exclude-launch-settings --no-https -n AspNetCore4WebApi -o AspNetCoreNet7Touch/AspNetCore4WebApi/src
+dotnet sln GSA2TENET4StarTrek.dotNetFoundationTouch.sln add -s AspNetCoreNet7Touch AspNetCoreNet7Touch/AspNetCore4WebApi/src
+
+dotnet new grpc -lang "C#" -F net7.0 --exclude-launch-settings -n AspNetCore4gRPC -o AspNetCoreNet7Touch/AspNetCore4gRPC/src
+dotnet sln GSA2TENET4StarTrek.dotNetFoundationTouch.sln add -s AspNetCoreNet7Touch AspNetCoreNet7Touch/AspNetCore4gRPC/src
+
+
+
+@echo.
+@echo.
+@echo.
 @echo\&echo  ---------- GenericHostTouch ----------
 dotnet new console -lang "C#" -f net5.0 -n GenericHost4App01 -o GenericHostTouch/GenericHost4App01/src
 dotnet sln GSA2TENET4StarTrek.dotNetFoundationTouch.sln add -s GenericHostTouch GenericHostTouch/GenericHost4App01/src
